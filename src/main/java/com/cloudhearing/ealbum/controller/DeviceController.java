85f3c8e54eaaf68307818c05e725baadbc26e0b6
zunly@hotmail.com <zunly@hotmail.com>
2019-10-04 07:50:44 +0800

    device部分差不多了

--- src/main/java/com/cloudhearing/ealbum/controller/DeviceController.java
+++ src/main/java/com/cloudhearing/ealbum/controller/DeviceController.java
@@ -4,25 +4,41 @@ import com.cloudhearing.ealbum.entity.Device;
 import com.cloudhearing.ealbum.service.DeviceService;
 import com.cloudhearing.ealbum.utils.JsonMsg;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-public class DeviceController extends BaseController{
+public class DeviceController extends BaseController {
     @Autowired
     DeviceService deviceService;
 
-    @PostMapping("/devices")
+    @GetMapping("/devices/")
     public JsonMsg listAllDevices() {
-        List<Device> deviceList=deviceService.listAll();
+        List<Device> deviceList = deviceService.listAll();
 
-        JsonMsg jsonMsg=feedbackJson(deviceList);
+        JsonMsg jsonMsg = feedbackJson(deviceList);
+
+        return jsonMsg;
+    }
+
+    @RequestMapping(value = "/devices/", method = RequestMethod.PUT)
+    public JsonMsg updateDevice(Device device) {
+
+        JsonMsg jsonMsg = feedbackJson(deviceService.updateDevice(device));
+
+        return jsonMsg;
+    }
+
+    @PostMapping("/devices/")
+    public JsonMsg addDevice(Device device) {
+
+        JsonMsg jsonMsg = feedbackJson(deviceService.addDevice(device));
 
         return jsonMsg;
 
     }
+
+
 }
